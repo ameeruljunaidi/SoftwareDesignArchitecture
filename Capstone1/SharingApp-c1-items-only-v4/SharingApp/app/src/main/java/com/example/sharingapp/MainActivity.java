95ae7a522fe36e4b9ab70be0aef058903c8f9b39
AJ Junaidi <ameerul.junaidi@gmail.com>
2022-03-24 21:22:39 -0400

    Revert "Tried doing manually"
    This reverts commit d4f5afbec66c9a54e768fcf6367ce9e28450ab47.

--- Capstone1/SharingApp-c1-items-only-v4/SharingApp/app/src/main/java/com/example/sharingapp/MainActivity.java
+++ Capstone1/SharingApp-c1-items-only-v4/SharingApp/app/src/main/java/com/example/sharingapp/MainActivity.java
@@ -2,17 +2,12 @@ package com.example.sharingapp;
 
 import android.content.Intent;
 import com.google.android.material.tabs.TabLayout;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 /**
  * Home Activity of the App
@@ -42,29 +37,5 @@ public class MainActivity extends AppCompatActivity {
     public void addItemActivity(View view) {
         Intent intent = new Intent(this, AddItemActivity.class);
         startActivity(intent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Called when MainActivity is started. Links the menu resource file menu_main.xml to MainActivity and
-        // "Inflates" the menu
-        // Inflate the menu; this adds items to the action bar if it is present
-        getMenuInflater().inflate(R.menu.menu_main, menu); // Menu resource, Menu
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        // Called when the user selects an option from the mnu
-        // Code handles what happens when the user selects the "Contacts" option from the menu - ContactsActivity is
-        // started
-        switch (item.getItemId()) {
-            case R.id.contacts:
-                Intent intent = new Intent(this, ContactActivity.class);
-                return true;
-            default:
-                return super.OnOptionsItemSelected(item);
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
